f7dd6ff215a5b93d5e5e2b22bafb36b75b276549
Adrian <c.adga@tessa.com.uy>
2020-10-27 18:12:25 -0300

    Practica de Listas

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # StudentScores_Colecciones
 C#
+
+- 1. Declarar e inicializar arreglo con nombres de estudiantes string[] studentNames 
+- 2. Declarar una lista ordenada para guardar los nombres y calificaciones.  
+- 3. Agrega el nombre del estudiante (ítem) y su calificación(score) a sortedStudentScores.
+- 4. Iiterar la lista sortedStudentScores y pintarla en pantalla (usa un foreach)
---
+++ StudentScores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentNames = {"Roberto","Jprge","Pablo"};
+            SortedList<string, int> sortedStudentScores = new SortedList<string, int>();
+            int score;
+
+            foreach (var item in studentNames)
+            {
+                score = 0;
+
+                while (score == 0)
+                {
+                    Console.WriteLine($"Proporciona la calificación de {item}");
+                    GetInteger(Console.ReadLine(), out score);
+                    sortedStudentScores.Add(item,score);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            foreach (var item in sortedStudentScores)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void GetInteger(string data, out int score)
+        {
+            if (!int.TryParse(data, out score))
+            {
+                Console.WriteLine("El dato debe ser entero");
+            }
+        }
+    }
+}
